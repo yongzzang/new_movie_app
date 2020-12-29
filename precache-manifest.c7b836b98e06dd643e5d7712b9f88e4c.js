6dd42a7f1581dc9e54368d65c56940e34e1cc31a
yongzzang <lyonggeon@gmail.com>
2020-12-29 21:21:55 +0900

    Updates

---
+++ precache-manifest.c7b836b98e06dd643e5d7712b9f88e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd876132de089cfdabe93cc823b450d",
+    "url": "/new_movie_app/index.html"
+  },
+  {
+    "revision": "79e11cca72e59ff6f443",
+    "url": "/new_movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/new_movie_app/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "79e11cca72e59ff6f443",
+    "url": "/new_movie_app/static/js/main.6ca46dc2.chunk.js"
+  },
+  {
+    "revision": "98ed438a8f8b2e98c1d7",
+    "url": "/new_movie_app/static/js/runtime~main.8d22dcbd.js"
+  }
+]);
